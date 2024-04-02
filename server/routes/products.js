a4ce956f9bff4ce89e637ec6a3e0cf1eb1f1b038
HALLakmina <lahirulakmina1999@gmail.com>
2024-04-02 09:27:58 +0530

    modified products file there are updating, delete and add products backend funtion

--- server/routes/products.js
+++ server/routes/products.js
@@ -11,7 +11,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
+    // try{console.log("runOut")}catch(err){return res.status(500, err).json()}
+    console.log("runOut")
     try{
+        console.log("runIn")
         const id = req.params.id;
         const product = DUMMY_PRODUCT_LIST.find((item) => item._id === id)
         if(product){
@@ -23,23 +26,43 @@ router.get('/:id', function(req, res, next) {
         return res.status(500, err).json()
     }
 });
-
-router.put('/:id', function(req, res, next) {
+router.post('/', function(req, res, next) {
     try{
-        const name = req.params.name;
-        const description = req.params.description;
-        const price = req.params.price;
-        const timestamp = req.params.timestamp;
-        const id = req.params.id;
+        const id = Math.random();
+        const name = req.query.name;
+        const description = req.query.description;
+        const price = req.query.price;
+        const timestamp = req.query.timestamp;
 
-        const product = DUMMY_PRODUCT_LIST.updateOne(
-            {_id : id}, {$set: 
-                {   name: name,
-                    description: description,
-                    price: price,
-                    timestamp: timestamp
+        const product = DUMMY_PRODUCT_LIST.push({"name":name, "description":description, "price":price, "timestamp":timestamp, "_id":id});
+        if(product){
+            return res.status(200).json(product)
+        }
+        else{
+            return res.status(404).json()
+        }
+    }catch(err){
+        return res.status(500, err).json()
+    }
+});
+router.put('/:id', function(req, res, next) {
+    console.log("run")
+    try{
+        const id = req.params.id;
+        const name = req.query.name;
+        const description = req.query.description;
+        const price = req.query.price;
+        const timestamp = req.query.timestamp;
+        const product = DUMMY_PRODUCT_LIST.map(index=>{
+            if(index._id === id){
+                {
+                    index.name = name,
+                    index.description = description,
+                    index.price = price,
+                    index.timestamp = timestamp
                 }
-            });
+            }
+        });
 
         if(product){
             return res.status(200).json(product)
@@ -52,10 +75,20 @@ router.put('/:id', function(req, res, next) {
     }
 })
 
-router.put('/:id', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
     try{
         const id = req.params.id;
-        const product = DUMMY_PRODUCT_LIST.deleteOne({_id:id})
+        let count=0;
+        const product = DUMMY_PRODUCT_LIST.map((index, item) =>{
+            if(index._id === id)
+            {
+                console.log(count," === ",id);
+                return(
+                    DUMMY_PRODUCT_LIST.splice(count, 1)
+                )
+            }
+            count = count+1
+        })
         if(product){
             return res.status(200).json(product)
         }
