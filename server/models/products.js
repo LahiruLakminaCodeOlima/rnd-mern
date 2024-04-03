5fc2969f00c565238dade31ad57450a02f9661b8
HALLakmina <lahirulakmina1999@gmail.com>
2024-04-03 15:18:30 +0530

    modified producte files to data validation

--- server/models/products.js
+++ server/models/products.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({ 
-    _id: String,
     name: String,
     description: String,
     price: Number,
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,5 @@
 var express = require('express');
-var express = require('express');
 var router = express.Router();
-const { DUMMY_PRODUCT_LIST } = require('../dummy/dummy-products');
 var Products = require('../models/products')
 
 router.get('/', async (req, res, next) => {
@@ -17,7 +15,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) =>{
     try{
         const id = req.params.id;
-        const product = await Products.findOne({_id : id})
+        const product = await Products.findOne({_id : id}).exec();
         if(product){
             return res.status(200).json(product)
         }else{
@@ -27,22 +25,42 @@ router.get('/:id', async (req, res, next) =>{
         return res.status(500, err).json()
     }
 });
+
 router.post('/', async (req, res, next) => {
     try{
-        const id = Math.random().toString();
-        const name = req.body.name;
-        const description = req.body.description;
-        const price = req.body.price;
-        const timestamp = req.body.timestamp;
-        console.log(id, name, description, price, timestamp)
-        const product = new Products({ "_id":id, "name":name, "description":description, "price":price, "timestamp":timestamp});
-        await product.save()
-        if(product){
-            return res.status(200).json(product)
+        const {name, description, price} =req.body;
+        const timestamp =new Date().toUTCString();
+        if(name)
+        {
+            if(description)
+            {
+                if(price && !isNaN(price))
+                {
+                    const product = new Products({"name":name, "description":description, "price":price, "timestamp":timestamp});
+                    await product.save()
+                    if(product)
+                    {
+                        return res.status(200).json(product)
+                    }
+                    else
+                    {
+                        return res.status(400).json()
+                    }
+                }
+                else
+                {
+                    return res.status(400).json("Price is not defined or is not a number")
+                }
+            }
+            else
+            {
+                return res.status(400).json("Description is not defined")
+            }
         }
-        else{
-            return res.status(404).json()
-        }
+        elses
+        {
+            return res.status(400).json("Name is not defined")
+        }        
     }catch(err){
         return res.status(500, err).json()
     }
@@ -51,20 +69,48 @@ router.put('/:id', async(req, res, next) => {
     
     try{
         const id = req.params.id;
-        const name = req.body.name;
-        const description = req.body.description;
-        const price = req.body.price;
-        const timestamp = req.body.timestamp;
+        const {name, description, price, timestamp} = req.body;
         //findOneAndUpdate
         //findByIdAndUpdate
         //updateOne
-        const product = await Products.updateOne({_id:id},{name : name, description : description, price : price, timestamp : timestamp});
-        if(product){
-            return res.status(200).json(product)
+        if(name)
+        {
+            if(description)
+            {
+                if(price && !isNaN(price))
+                {
+                    if(timestamp)
+                    {
+                        
+                        const product = await Products.updateOne({_id:id},{name : name, description : description, price : price, timestamp : timestamp});
+                        if(product)
+                        {
+                            return res.status(200).json(product)
+                        }
+                        else
+                        {
+                            return res.status(404).json()
+                        }
+                    }
+                    else
+                    {
+                        return res.status(400).json("Timestamp is not defined")
+                    }
+                }
+                else
+                {
+                    return res.status(400).json("Price is not defined or is not a number")
+                }
+            }
+            else
+            {
+                return res.status(400).json("Description is not defined")
+            }
         }
-        else{
-            return res.status(404).json()
-        }
+        elses
+        {
+            return res.status(400).json("Name is not defined")
+        } 
     }catch(err){
         return res.status(500, err).json()
     }
